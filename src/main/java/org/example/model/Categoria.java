7f6bae7c9fc82459079771314bebb71f1ae325e4
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-11-06 20:42:52 -0300

    feat(model): Criacao de classe responsavel por Origem no banco

--- src/main/java/org/example/model/Categoria.java
+++ src/main/java/org/example/model/Categoria.java
@@ -1,4 +1,38 @@
 package org.example.model;
 
 public class Categoria {
+    private int id;
+    private String nome;
+    private String tipoMovimento;
+
+    public Categoria(String nome, String tipoMovimento) {
+        setNome(nome);
+        setTipoMovimento(tipoMovimento);
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setTipoMovimento(String tipo){
+        this.tipoMovimento = tipo;
+    }
+
+    public String getTipoMovimento(){
+        return this.tipoMovimento;
+    }
+
+    @Override
+    public String toString() {
+        String mensagem = "Id: " + this.id + "\nNome: " + this.nome + "\nTipo de movimento: " + this.tipoMovimento;
+        return mensagem;
+    }
 }
--- src/main/java/org/example/model/Origem.java
+++ src/main/java/org/example/model/Origem.java
@@ -1,4 +1,38 @@
 package org.example.model;
 
 public class Origem {
+    private int id;
+    private String nome;
+    private String tipo;
+
+    public Origem(String nome, String tipo){
+        setNome(nome);
+        setTipo(tipo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    @Override
+    public String toString() {
+        String mensagem = "Id: " + this.id + "\nNome: " + this.nome + "\nTipo: " + this.tipo;
+        return mensagem;
+    }
 }

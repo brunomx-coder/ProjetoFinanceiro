b9622b8beb8f0d3cd66745e33a68aa682435da3e
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-11-06 20:43:35 -0300

    feat(model): Criacao de classe responsavel por Transacao no banco

--- src/main/java/org/example/model/Transacao.java
+++ src/main/java/org/example/model/Transacao.java
@@ -1,4 +1,93 @@
 package org.example.model;
 
+import java.util.Date;
+
 public class Transacao {
+    private int id;
+    private Date data;
+    private String descricao;
+    private String tipo;
+    private String parcela;
+    private String fixaOuVariavel;
+    private double valor;
+    Origem origem;
+    Categoria categoria;
+
+    public Transacao(Date data, String descricao, String tipo, String fixaOuVariavel, double valor, String nomeCategoria, String tipoCategoria, String nomeOrigem, String tipoOrigem){
+        setData(data);
+        setDescricao(descricao);
+        setTipo(tipo);
+        setFixaOuVariavel(fixaOuVariavel);
+        setValor(valor);
+        setCategoria(nomeCategoria, tipoCategoria);
+        setOrigem(nomeOrigem, tipoOrigem);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getParcela() {
+        return parcela;
+    }
+
+    public void setParcela(String parcela) {
+        this.parcela = parcela;
+    }
+
+    public String getFixaOuVariavel() {
+        return fixaOuVariavel;
+    }
+
+    public void setFixaOuVariavel(String fixaOuVariavel) {
+        this.fixaOuVariavel = fixaOuVariavel;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setOrigem(String nome, String tipo){
+        origem = new Origem(nome, tipo);
+    }
+
+    public Origem getOrigem() {
+        return this.origem;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String nome, String tipo) {
+        this.categoria = new Categoria(nome, tipo);
+    }
 }

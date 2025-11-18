9b0c5b2259937ddc006e727fcff7214358b52cf8
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-11-18 07:32:40 -0300

    feat(repository): Criacao interface repository para classe Transacao

--- src/main/java/org/example/repository/TransacaoRepository.java
+++ src/main/java/org/example/repository/TransacaoRepository.java
@@ -1,4 +1,17 @@
 package org.example.repository;
 
-public class TransacaoRepository {
+import org.example.model.Transacao;
+import java.util.List;
+import java.util.Optional;
+import java.util.Date;
+
+public interface TransacaoRepository {
+    Transacao save(Transacao transacao);
+    Optional<Transacao> findById(Integer id);
+    Optional<Transacao> findByTipo(String tipo);
+    Optional<Transacao> findByData(Date data);
+    List<Transacao> findAll();
+    void deletedById(Integer id);
+
+    Optional<Transacao> findByDescricao(String descricao);
 }

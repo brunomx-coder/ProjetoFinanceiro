84da9f77e3bb0e94fd34616e470ce48c729b03cf
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-11-18 07:27:29 -0300

    feat(repository): Criacao interface repository para classe Categoria

--- src/main/java/org/example/repository/CategoriaRepository.java
+++ src/main/java/org/example/repository/CategoriaRepository.java
@@ -1,4 +1,16 @@
 package org.example.repository;
 
-public class CategoriaRepository {
+import org.example.model.Categoria;
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoriaRepository {
+    Categoria save(Categoria categoria);
+    Optional<Categoria> findById(Integer id);
+    Optional<Categoria> findByTipo(String tipo);
+    List<Categoria> findAll();
+    void deletedById(Integer id);
+
+    Optional<Categoria> findByDescricao(String descricao);
+
 }

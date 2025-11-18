6ef1cf4d3830fd2e8393694256b7b6af11c623d9
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-11-18 07:22:26 -0300

    feat(repository): Criacao interface repository para classe Origem

--- src/main/java/org/example/repository/OrigemRepository.java
+++ src/main/java/org/example/repository/OrigemRepository.java
@@ -1,4 +1,17 @@
 package org.example.repository;
 
-public class OrigemRepository {
+import org.example.model.Origem;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrigemRepository {
+    //CRUD Básico
+    Origem save(Origem origem);
+    Optional<Origem> findById(Integer id);
+    Optional<Origem> findByTipo(String tipo);
+    List<Origem> findAll();
+    void deletedById(Integer id);
+
+    //Método personalizado de busca
+    Optional<Origem> findByDescricao(String descricao);
 }
